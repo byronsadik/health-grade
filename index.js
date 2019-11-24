60a4b29679bba83cf4fab8a5d0d0e72966dfcefd
Byron <byron@attoconsulting.com>
2019-11-24 15:37:35 -0500

    axios works to get json data from api, still can't render in browser though

--- index.js
+++ index.js
@@ -1,46 +1,56 @@
 const express = require('express');
 const app = express();
-const port = 4000;
-const fetch = require('node-fetch');
-const querystring = require('querystring');
+const port = 3001;
+
+const axios = require('axios');
+
+const util = require('util');
+
 
 
 
 app.get('/', (req, res) => {
 
-    // res.send('Hello World!');
-
-
-// "https://drrobotmck-nyc-health-inspection-results-v1.p.rapidapi.com/restaurants?dba=dunkin%20donuts&zip_code=11213&boro=brooklyn"
-
-    let params = {
-            "method": "GET",
-            "$limit" : 20,
+    axios.get('https://data.cityofnewyork.us/resource/43nn-pn8j.json', {
+        params: {
+            "$limit" : 10,
             "$$app_token" : "rwWEn2Tw493ASSX8bzGjwuz8O",
-            "zipcode": 11213
-        };
+            "zipcode": 11213    
+        }
+      })
+      .then(function (response) {
+        console.log(response);
+        res.json(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 
 
-    let urString;
-
-    urString = 'https://data.cityofnewyork.us/resource/43nn-pn8j.json?' + querystring.stringify(params);
-
-    console.log(urString);
-
-
-
-    fetch(urString, {
-        
-    })
-        .then(response => {
-            console.log(response);
-            res.json(response);
-        })
-        .catch(err => {
-            console.log(err);
-        });
-
-
-    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+
+
+
+
+function ajaxCall(){
+
+    $.ajax({
+        url: "https://data.cityofnewyork.us/resource/43nn-pn8j.json",
+        type: "GET",
+        data: {
+          "$limit" : 20,
+          "$$app_token" : "rwWEn2Tw493ASSX8bzGjwuz8O",
+          "zipcode": 11213
+        }
+    }).done(function(data) {
+      // alert("Retrieved " + data.length + " records from the dataset!");
+      console.log(data);
+
+      res.json(data);
+    });
+
+}
