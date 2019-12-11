267e9a03cae88934536e5ab4955d3477a46e28b0
Byron <byron@attoconsulting.com>
2019-12-10 23:26:53 -0500

    pushing to master

--- index.js
+++ index.js
@@ -1,56 +1,53 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 4000;
 
 const axios = require('axios');
 
 const util = require('util');
+const fetch = require("node-fetch");
 
+const cors = require('cors');
 
-
+app.use(cors());
 
 app.get('/', (req, res) => {
 
-    axios.get('https://data.cityofnewyork.us/resource/43nn-pn8j.json', {
-        params: {
-            "$limit" : 10,
-            "$$app_token" : "rwWEn2Tw493ASSX8bzGjwuz8O",
-            "zipcode": 11213    
-        }
-      })
-      .then(function (response) {
-        console.log(response);
-        res.json(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-
+  getZipCode(res);
 
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
+function getZipCode(res) {
+   axios.get('https://ipapi.co/json/')
+          .then((response) => {
+            getRestaurants(response.data.postal, res);
+          })
+          .catch((error) => {
+            console.log(error);
+          }); // end API call
+}
 
+function getRestaurants(zip, res) {
+  axios.get('https://data.cityofnewyork.us/resource/43nn-pn8j.json', {
+            params: {
+                "$limit" : 10,
+                "$$app_token" : "rwWEn2Tw493ASSX8bzGjwuz8O",
+                "zipcode": zip  // should be this.state.zip
+            }
+          })
+          .then((response) => {
 
+            // debugger;
 
+            console.log('returning response data');
+            res.send(response.data);
+            // return the thingie up above somehow.
 
-function ajaxCall(){
-
-    $.ajax({
-        url: "https://data.cityofnewyork.us/resource/43nn-pn8j.json",
-        type: "GET",
-        data: {
-          "$limit" : 20,
-          "$$app_token" : "rwWEn2Tw493ASSX8bzGjwuz8O",
-          "zipcode": 11213
-        }
-    }).done(function(data) {
-      // alert("Retrieved " + data.length + " records from the dataset!");
-      console.log(data);
-
-      res.json(data);
-    });
-
+          })
+          .catch((error) => {
+            console.log(error);
+          }); // end API call
 }
